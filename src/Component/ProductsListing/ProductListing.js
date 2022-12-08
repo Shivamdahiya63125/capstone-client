51740ad3991bf588545e31e6ec3b790b99bc2508
shivam <shivamdahiya63125@gmail.com>
2022-12-07 22:52:21 -0500

    test

--- src/Component/ProductsListing/ProductListing.js
+++ src/Component/ProductsListing/ProductListing.js
@@ -11,7 +11,10 @@ const ProductListing = () => {
       mode: "cors",
       method: "GET",
     };
-    await fetch("http://localhost:8080/listing/getalllisting", requestOptions)
+    await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/listing/getalllisting`,
+      requestOptions
+    )
       .then((response) => {
         return response.json();
       })
